db64226d6be8d7a76206c7e83bb8bc8ec79f3688
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-03-29 16:16:52 +0530

    style - placeholder text color change

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -92,7 +92,7 @@ function Search() {
               </svg>
             </span>
             <input
-              className="placeholder:italic placeholder:text-slate-400 block bg-darkGrey w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm mb-4 focus:outline-none focus:border-slate-500 focus:ring-slate-500 focus:ring-1 sm:text-sm"
+              className="placeholder:italic placeholder:text-slate-400 text-slate-400 block bg-darkGrey w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm mb-4 focus:outline-none focus:border-slate-500 focus:ring-slate-500 focus:ring-1 sm:text-sm"
               placeholder="Search for anything..."
               type="text"
               name="search"
